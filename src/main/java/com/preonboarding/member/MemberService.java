b6cacff6e5b46126285f1bfc7b807c99b996de51
yeonssu <dustn7197@naver.com>
2023-08-09 18:56:40 +0900

    refactor : Transactional annotation 추가

--- src/main/java/com/preonboarding/member/MemberService.java
+++ src/main/java/com/preonboarding/member/MemberService.java
@@ -6,8 +6,10 @@ import com.preonboarding.global.utils.AuthorityUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class MemberService {
 
